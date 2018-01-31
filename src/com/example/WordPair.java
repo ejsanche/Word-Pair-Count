f667ca0687977edc3bc116e4f4dbe1eb4dc2e756
Rishi Ranjan <34177572+rishir95@users.noreply.github.com>
2018-01-31 11:46:18 -0500

    Update WordPair.java

--- src/com/example/WordPair.java
+++ src/com/example/WordPair.java
@@ -16,24 +16,26 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+
+
 public class WordPair {
     public static void main(String [] args) throws Exception
     {
-        Configuration c=new Configuration();
-        String[] files=new GenericOptionsParser(c,args).getRemainingArgs();
+        Configuration conf=new Configuration();
+        String[] files=new GenericOptionsParser(conf,args).getRemainingArgs();
         Path input=new Path(files[0]);
         Path output=new Path(files[1]);
 
         @SuppressWarnings("deprecation")
-        Job j=new Job(c,"word pair");
-        j.setJarByClass(WordPair.class);
-        j.setMapperClass(MapForWordCount.class);
-        j.setReducerClass(ReduceForWordCount.class);
-        j.setOutputKeyClass(Text.class);
-        j.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(j, input);
-        FileOutputFormat.setOutputPath(j, output);
-        System.exit(j.waitForCompletion(true)?0:1);
+        Job job=new Job(conf,"word pair");
+        job.setJarByClass(WordPair.class);
+        job.setMapperClass(MapForWordCount.class);
+        job.setReducerClass(ReduceForWordCount.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, input);
+        FileOutputFormat.setOutputPath(job, output);
+        System.exit(job.waitForCompletion(true)?0:1);
     }
     public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable>{
         public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
@@ -42,7 +44,6 @@ public class WordPair {
             String temp3=new String();
             String temp2=new String();
             String temp=new String();
-//            String[] words=line.split(" ");
 
             StringTokenizer itr = new StringTokenizer(line);
 
